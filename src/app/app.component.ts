b3424a24ed51c316a80b6eb472226d12ca48dac1
Douglas Ramirez <dorz.ram@gmail.com>
2020-05-22 20:36:04 -0400

    Desing improvements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,16 +8,16 @@ import { Component } from '@angular/core';
 export class AppComponent {
   //---------------------------------------------------------------------
   title = 'Yiado';
-  quien = 'NO SABEMOS QUIENES SOMOS';
-  donde = 'NO SABEMOS QUE QUEREMOS';
-  que = 'NO SABEMOS A DONDE VAMOS';
+  quien = '   ¿?';
+  donde = '   ¿?';
+  que = '   ¿?';
   //---------------------------------------------------------------------
-  aboutTitle = 'Como lo hacemos?';
+  aboutTitle = 'Cómo lo hacemos?';
   aboutText1 = 'En #Yiado pensamos que el desarrollo de software no solo debe ser escritura de codigo';
   aboutText2 = ' <!-- HOLA MUNDO --> nuestro cultura es el desarrollo de ideas.';  
   aboutIdea = 'Idea';
   aboutConcept = 'Concepto';
-  aboutDesing = 'Desiño';
+  aboutDesing = 'Diseño';
   aboutDevelop = 'Desarrollo';
 
 
