5a5cc6ee00bf7ded38f974538a7cdd20d7f0bf60
Douglas Ramirez <dorz.ram@gmail.com>
2020-06-02 00:29:45 -0400

    Mejoras visuales y depuracion de codigo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -30,9 +30,9 @@ export class AppComponent  implements OnInit {
   aboutIdea = 'Idea';
   aboutIdeaDescrip = 'Experimentar, hacer cosas nuevas, ser curiosos, no tener miedo, básicamente volver a ser niños y dejar volar nuestra imaginación es nuestra cultura.';
   aboutConcept = 'Concepto';
-  aboutConceptDescrip = 'Transmitimos la esencia de una idea, fijamos objetivos, para que y/o por que genera valor a la sociedad.';
+  aboutConceptDescrip = 'Transmitimos la esencia de una idea, fijamos objetivos, para que o por qué genera valor a la sociedad.';
   aboutDesing = 'Diseño';
-  aboutDesingDescrip = 'Más que bocetos o dibujos, creamos comunicación visual, trazamos ideas en colores, procesos, indentificamos como impactar con creatividad.';
+  aboutDesingDescrip = 'Más que bocetos o dibujos, creamos comunicación visual, trazamos ideas en colores, procesos, identificamos como impactar con creatividad.';
   aboutDevelop = 'Desarrollo';
   aboutDevelopDescrip = "Materializamos la creatividad y abstracción de un producto o proceso, llevamos a operacion ideas convertidas en oportunidades.";
   //----------------------------------------------------------------------
@@ -45,29 +45,29 @@ export class AppComponent  implements OnInit {
   featuresDigital = 'Transformación digital Community Manager';
   featuresDigitalDescrip = 'Ayudamos a definir la estrategia para transformar tu negocio y alcanzar la madurez digital.';
   //----------------------------------------------------------------------
-  cloudTitle = '¿En la Nuve?';
-  cloudDescrip = 'Somos tu apoyo para tomar la mejor decision al integrar servicios en la nuve en el desarrollo de tus ideas, desde tu pagina web, E-Commerce o App, contamos con experiencia en Amazon Web Services, Google Cloud que te permitiran escalar tu desarrollo con un excelente costo.';
+  cloudTitle = '¿En la Nube?';
+  cloudDescrip = 'Somos tu apoyo para tomar la mejor decision al integrar servicios en la nube en el desarrollo de tus ideas, desde tu pagina web, E-Commerce o App, contamos con experiencia en Amazon Web Services, Google Cloud que te permitiran escalar tu desarrollo con un excelente costo.';
   cloudSeg = "Proporciona un control completo sobre los recursos informáticos y le permite ejecutarse en el entorno informático";
   cloudSto = "Almacenamiento y procesamiento de recursos informaticos ofreciendo escalabilidad, disponibilidad de datos, seguridad y rendimiento";
-  cloudZone = 'Asesoramiento al implementar tu estrategia para ejecutar tu aplicaciones empresariales en regiones y zonas de disponibilidad que se ajuten a tu prioridad.';
+  cloudZone = 'Asesoramiento al implementar tu estrategia para ejecutar tus aplicaciones empresariales, en regiones y zonas de disponibilidad que se ajusten a tu prioridad.';
   cloudpricing = 'Paga por los servicios individuales que necesite durante el tiempo que los use, sin contratos a largo plazo ni licencias complejas.';
   //----------------------------------------------------------------------
   team = "Nuestro Equipo"; 
   teamDescrip = "Somos un equipo multidisciplinario emprendedor, amante de la tecnología, que esta preparado para asumir nuevos desafios."
-  devLram = "Liliana Suarez";
+  devLram = "Liliana Suárez";
   devDensing = "Designer - Community Manager";
-  devDorz = "Douglas Ramirez";
+  devDorz = "Douglas Ramírez";
   devFront = "Front-end Developer   ";
-  devYsmm = "Ysrael Mendez";
+  devYsmm = "Ysrael Méndez";
   devBack = "Back-end Developer     ";
 //-------------------------------------------------------------------------
   footerTitle = "#YiaDo";
   footerDescrip = "Si lo puedes #Imaginar, lo puedes #Programar."; 
-  footerDirec = "Direccion";
+  footerDirec = "Dirección";
   footerDirecDescrip = "Hugo Silva Endeiza 840, Antofagasta, Chile.";
-  footerTelef = "Telefono";
+  footerTelef = "Teléfono";
   footerTelefDescrip1 = "(+56) 9 55174678";
-  footerTelefDescrip2 = "(+56) 9 53932764";
+  footerTelefDescrip2 = "(+56) 9 97625414";
   footerNetwork = "Contacto";
   footerNetworkDecrip1 = "contacto@yiado.cl";
   footerNetworkDecrip2 = "github.com/yiado";
